cf1e7bcb9bbb56ff92fc41de510db8ab02b842c2
dev-demos <ejbgames@gmail.com>
2018-12-14 16:51:59 -0600

    Maven heroku plugin.

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-    @RequestMapping(value="/",method = RequestMethod.GET)
+    
+	private static final String PATH = "/";
+	
+	@RequestMapping(value=PATH,method = RequestMethod.GET)
     public String homepage(){
         return "index.html";
     }
